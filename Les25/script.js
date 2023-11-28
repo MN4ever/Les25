7f46b5a19e08e654d0e92ff3ac7907c5b8de15ff
MN4ever <144025235+MN4ever@users.noreply.github.com>
2023-11-28 20:59:16 +0100

    Add files via upload

---
+++ Les25/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+  $("#antwoord").hide();
+  $("#verberg").click(function () {
+    $("p").hide();
+  });
+  $("#toon").click(function () {
+    $("p").show();
+  });
+});
+
